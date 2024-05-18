3c410cbbb52c7197f44914587fbcc5b1c4d55707
ADITYA SINHA <126620648+47sinnha@users.noreply.github.com>
2024-05-18 10:28:56 +0000

    stringsandnumber

---
+++ basics/stringsandnumber.js
@@ -0,0 +1,13 @@
+console.log("1" + 2); 
+console.log(1+"2"); 
+console.log("1"+2+2); 
+console.log(1+2+"2"); 
+
+// Above is rule of ecma in which if first is string then all the things will be treated as string 
+// Otherwise if first is number then all the things will be treated as number 
+
+//Output of above:- 
+// 12
+// 12
+// 122
+// 32
