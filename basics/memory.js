43158c5bd9cc90cb1af4029ce83effd1e7efb4af
ADITYA SINHA <126620648+47sinnha@users.noreply.github.com>
2024-05-20 09:00:49 +0000

    added again

--- basics/memory.js
+++ basics/memory.js
@@ -9,8 +9,8 @@ let channelnames = "hitesh"
 let anothername = channelnames
 anothername = "Aditya"  
 
-console.log(channelnames);
-console.log(anothername);
+// console.log(channelnames);
+// console.log(anothername);
 
 /*
 In stack space a memory space is created in the name of 
@@ -23,5 +23,25 @@ which means we assign the copy of channelnames to anothername due to which when 
 make a  change original variable doesn't get affected.. 
 two stack area is created 1. channelnames
 2. anothername
+*/
+
+// Now let's take an example of non primitive data type 
+
+let userone = {
+    name:"Aditya", 
+    enroll: 2202030400198
+}
+
+let usertwo = userone
+usertwo.name="Ritik", 
+
+console.log(userone.name);
+console.log(usertwo.name);
+// Now let's what happens in the non prmitive data type case
+/*
+two memory space is created in stack space one as userone and another as 
+usertwo whether both are refren.. to heap memory in which name and enroll 
+are present are userone and usertwo are refering to them therefore change in 
+one can lead to changing the both 
 */
 
